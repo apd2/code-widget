db0919ff2de02eaadf01be3576bc3af4bbc73f0a
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2013-09-30 12:18:53 -0400

    fix

--- CodeWidgetAPI.hs
+++ CodeWidgetAPI.hs
@@ -94,7 +94,7 @@ codeRegionEditable ref r b = do
                                      let np = p {pgRegions = nx:ox}
                                      let op = otherPages cv (pgID p)
                                      writeIORef ref cv {cvPages = np:op}
-                                     cvSetEditFlags p 
+                                     cvSetEditFlags np 
                                      return ()
                             else error ("regionEditable: cannot change region with nested subregions")
                               
